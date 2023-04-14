a9c56c0fb204e613c08fab9c4098b918af249377
Pobitrodeb <pobitrodn@gmail.com>
2023-04-14 12:03:47 +0600

    compostion api setup done

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,5 +1,21 @@
 <template>
-
-    <h1>Congratulations </h1>
-
+  <h4> Course Name: {{ courseName }}</h4>
+  <p> Mentor: {{ mentor }}</p>
+  
+  <button @click="count++"> +</button>
+    {{ count }}
+  <button @click="count--"> - </button>
 </template>
+
+
+<script>
+export default {
+  setup() {
+    let count = 0; 
+    let courseName = "Vue.js Master Class";
+    let mentor = "Mahamudh Ibrahim";
+
+    return {count, courseName, mentor}
+  }
+}
+</script>
