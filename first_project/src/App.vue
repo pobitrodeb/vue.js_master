5bd408140523021c5218e5a1d5667d4d3267149d
Pobitrodeb <pobitrodn@gmail.com>
2023-04-14 13:58:58 +0600

    basic function delcear

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,26 +1,30 @@
+// Methods -function way  
 <template>
-  <input type="text" v-model="course.name">
-  <h4> Course Name: {{ course.name }}</h4>
-  <p> Mentor: {{ course.mentor }}</p>
-  <p> Platform: {{ course.platform }}</p>
-  
-  <button @click="count++"> +</button>
+  <div>
+    <h2> Course Name:  {{ course.name }}</h2>
+    <button @click="increment"> + </button>
     {{ count }}
-  <button @click="count--"> - </button>
-
-   <!-- Two Way Data Binding  -->
-  <p>Give your number </p>
-  <input type="number" v-model="count">
-
+    <button @click="decrement"> - </button>
+  </div>
 </template>
 
-// [ when your data non Primitive Type [object] you use reactive]
+
+
 <script setup>
-import {ref, reactive} from 'vue'; 
-let count = ref(0); 
-let course = reactive({
-      name: "Vue.js Master Class", 
-      mentor: "Musha Ibrahim", 
-      platform : "instructory", 
-    })
+  import {ref, reactive} from 'vue'; 
+  let count = ref(0); 
+  let course = reactive({
+    name: "Vue.js Master Class"
+  })
+
+  function increment()
+  {
+    count.value++
+  }
+
+  function decrement()
+  {
+    count.value--
+  }
+
 </script>
