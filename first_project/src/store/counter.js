f90fc6432dbab9602b488e3b74f8d8d748ee53c1
Pobitrodeb <pobitrodn@gmail.com>
2023-04-23 23:50:54 +0600

    pinia actions syantax added

--- first_project/src/store/counter.js
+++ first_project/src/store/counter.js
@@ -7,3 +7,19 @@ export const useCounter = defineStore('counter',{
         title: 'v3 master class', 
     })
 })
+
+// pinia basic function 
+export const useCounterStore = defineStore('counter', {
+    state: () => ({
+      count: 0,
+    }),
+    actions: {
+      // since we rely on `this`, we cannot use an arrow function
+      increment() {
+        this.count++
+      },
+      randomizeCounter() {
+        this.count = Math.round(100 * Math.random())
+      },
+    },
+  })
