a62eb63bcbccb30cdde9f427754d6d2b99f6577b
Pobitrodeb <pobitrodn@gmail.com>
2023-04-23 21:42:23 +0600

    userRouter and router push session done

--- first_project/src/views/Services.vue
+++ first_project/src/views/Services.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
         <h2> This is Service Page </h2>
+
+        <button @click="goHomePage"> Back To Home </button>
+
     </div>
 </template>
+
+
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter(); 
+const goHomePage = () => {
+    router.push({name: 'home'}); 
+}
+</script>
