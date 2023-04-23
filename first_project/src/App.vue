7452b49e6e41fdf947635924a968a3bba7d97af1
Pobitrodeb <pobitrodn@gmail.com>
2023-04-23 21:25:22 +0600

    Named routes session done

--- first_project/src/App.vue
+++ first_project/src/App.vue
@@ -1,10 +1,19 @@
 <template>
 
-<div class="">
+<!-- <div class="">
   <a href="/"> Home | </a>
   <a href="/about"> About |  </a>
   <a href="/services"> Services |  </a>
   <a href="/contact"> Contact |  </a>
+</div> -->
+
+
+<!-- Named routes -->
+<div class="">
+  <router-link :to="{name: 'home'}"> Home |</router-link>
+  <router-link :to="{name: 'about'}"> About |</router-link>
+  <router-link :to="{name: 'services'}"> Services | </router-link>
+  <router-link :to="{name: 'contact'}"> Contact | </router-link>
 </div>
 
 <router-view/>
--- first_project/src/router/index.js
+++ first_project/src/router/index.js
@@ -7,10 +7,10 @@ import Services from '../views/Services.vue';
 import Contact from '../views/Contact.vue';
 
 const routes = [
-    { path: '/', component: Home },
-    { path: "/about", component: About },
-    { path: "/services", component: Services },
-    { path: "/contact", component: Contact },
+    { path: '/',         name:'home',     component: Home },
+    { path: "/about",    name:'about',    component: About },
+    { path: "/services", name:'services', component: Services },
+    { path: "/contact",  name:'contact',  component: Contact },
 ];
 
 const router = createRouter({
