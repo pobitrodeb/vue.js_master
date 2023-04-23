862969fc2c661ab5572ab5674985f232ee886155
Pobitrodeb <pobitrodn@gmail.com>
2023-04-23 23:55:26 +0600

    using pinia actions

--- first_project/src/store/counter.js
+++ first_project/src/store/counter.js
@@ -5,21 +5,11 @@ export const useCounter = defineStore('counter',{
     state: () => ({
         count: 0,
         title: 'v3 master class', 
-    })
-})
-
-// pinia basic function 
-export const useCounterStore = defineStore('counter', {
-    state: () => ({
-      count: 0,
     }),
+
     actions: {
-      // since we rely on `this`, we cannot use an arrow function
-      increment() {
-        this.count++
-      },
-      randomizeCounter() {
-        this.count = Math.round(100 * Math.random())
-      },
-    },
-  })
+        increments(n){
+                this.count  += n
+        }
+    }
+});
