078b264ea4aeb6121c4e9823365473ca0bc313e4
Pobitrodeb <pobitrodn@gmail.com>
2023-04-24 23:14:47 +0600

    Todo App UI Design using Bootstrap session done

--- todo-app/src/App.vue
+++ todo-app/src/App.vue
@@ -2,14 +2,42 @@
 </script>
 
 <template>
-  <div class="container">
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur
-      suscipit hic optio dolores quo, eius tenetur dolorum consequatur totam
-      sapiente neque itaque esse repellat eos, voluptate corrupti, ducimus non
-      recusandae!
-    </p>
-  </div>
+  <section class="py-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card">
+            <div class="card-header text-center bg-primary text-white">
+              Vue.js Todo App | www.pobitrodeb.com
+            </div>
+            <div class="card-body">
+              <div class="card-title text-primary">Task List</div>
+              <div class="row">
+                <form>
+                  <div class="col-md-12">
+                    <label for="" class="form-label"> Add New Task </label>
+                    <input type="text" class="form-control" />
+                  </div>
+                  <br />
+                  <input type="submit" class="btn btn-primary" value="Submit" />
+                </form>
+              </div>
+            </div>
+          </div>
+
+          <div class="card my-2">
+            <div class="card-body">
+              <ul class="list-group d-flex justify-content-between">
+                <li class="list-group-item "> Task 1 
+                  <a href="" class="btn"> <i class="fa-solid fa-xmark">  </i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 
