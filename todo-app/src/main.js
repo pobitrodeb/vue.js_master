9c6263d303435992b60e89ce2558964d56c95259
Pobitrodeb <pobitrodn@gmail.com>
2023-04-24 18:06:08 +0600

    bootstrap setup done

--- todo-app/src/main.js
+++ todo-app/src/main.js
@@ -2,10 +2,18 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-import './assets/main.css'
+
+//  Custom CSS 
+import './assets/sass/style.scss'
+
+
 
 const app = createApp(App)
 
 app.use(createPinia())
 
 app.mount('#app')
+
+
+//bootstrap JS support 
+import * as bootstrap from 'bootstrap'
