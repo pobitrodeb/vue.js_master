b5a6eeb28b34f20ba8e06e1ee59263ab550c92be
Pobitrodeb <pobitrodn@gmail.com>
2023-04-26 23:30:51 +0600

    home page design

--- movi-app/src/App.vue
+++ movi-app/src/App.vue
@@ -1,18 +1,16 @@
 <template>
-<header>
-    <router-link :to="{name: 'home'}">
-      <h1>Movies </h1>
+  <header class="bg-primary text-center p-2">
+    <router-link :to="{ name: 'home' }">
+      <h1 class="text-white">Movie App </h1>
     </router-link>
 
-<main>
-  <router-view></router-view>
-</main>
-
+    <main>
+      <router-view></router-view>
+    </main>
   </header>
 </template>
 
 <script lang="ts" setup>
-
 </script>
 
 <style lang="scss" scoped>
