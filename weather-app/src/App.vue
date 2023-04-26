2fadb35666a606a4cda2280a531812d74b82263f
Pobitrodeb <pobitrodn@gmail.com>
2023-04-26 14:27:27 +0600

    Weather App - UI Design session done

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,47 +1,60 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <h2 class="bg-primary text-white py-2 text-center">Weather App With Vue.js | www.pobitrodeb.com </h2>
+  <div class="main-weather text-center">
+  
+    <!-- warther location search  -->
+    <div class="search-box p-3">
+      <input type="text" class="form-control" placeholder="search location"/>
     </div>
-  </header>
+    <!-- weather-info  -->
+    <div class="weather-info">
+      <div class="location-box">
+        <div class="location">Dhaka</div>
+        <div class="date">26 Aprril 2023</div>
+      </div>
+    </div>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <div class="weather-box">
+      <div class="temp">22 °C</div>
+      <div class="weather">habi jabi</div>
+    </div>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script setup>
+</script>
+
+<style  scoped>
+@import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap");
+* {
+  box-sizing: border-box;
+  font-family: "Montserrat", sans-serif;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+h2{
+  font-weight: 800;
 }
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.main-weather {
+  margin: 0px auto;
+  background-image: url("./assets/defult.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: 0.4s;
+  width: 320px;
+  height: 480px;
+  color: white;
+  border-radius: 25px;
+}
+.location{
+  font-size: 28px;
+  font-weight: bold;
+}
+.date{
+  font-weight: 800;
+}
+.temp{
+  color:red; 
+  font-size: 24px;
+  font-weight: 600;
 }
 </style>
