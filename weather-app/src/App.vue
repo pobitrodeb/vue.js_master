c66023380676e20cd4cfb66dc74706392bd7714b
Pobitrodeb <pobitrodn@gmail.com>
2023-04-26 17:20:47 +0600

    not response fetchweather function | weather.js file

--- weather-app/src/App.vue
+++ weather-app/src/App.vue
@@ -1,28 +1,46 @@
+
+<script setup>
+import {useWeatherStore} from './stores/weather'
+const weatherStore = useWeatherStore();
+
+</script>
+
+
 <template>
   <h2 class="bg-primary text-white py-2 text-center">Weather App With Vue.js | www.pobitrodeb.com </h2>
   <div class="main-weather text-center">
   
     <!-- warther location search  -->
     <div class="search-box p-3">
-      <input type="text" class="form-control" placeholder="search location"/>
+      <input type="text" class="form-control" placeholder="search location"
+      v-model="weatherStore.location_query"
+      @keypress="weatherStore.fetchweather"
+      />
     </div>
     <!-- weather-info  -->
     <div class="weather-info">
       <div class="location-box">
-        <div class="location">Dhaka</div>
-        <div class="date">26 Aprril 2023</div>
+        <div class="location">
+          {{ weatherStore.weather.name}}
+        </div>
+        <div class="date">
+          {{ new Date().toLocaleString()}}
+        </div>
       </div>
-    </div>
+    </div>   
 
     <div class="weather-box">
-      <div class="temp">22 °C</div>
-      <div class="weather">habi jabi</div>
+      <!-- <div class="temp"> {{ weatherStore.weather.main.temp }}°C</div> -->
+      <div class="temp"> 34 °C</div>
+      <!-- <div class="weather text-white">{{ weatherStore.weather.weather[0].main}}</div> -->
+      <div class="weather text-white">Hot Day </div>
+      <div class="icon">
+        <!-- <img :src="`https://openweathermap.org/img/wn/${ weatherStore.weather.weather[0].icon}@2x.png`" alt="icon"> -->
+        <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="icon">
+      </div>
     </div>
   </div>
 </template>
-
-<script setup>
-</script>
 
 <style  scoped>
 @import url("https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap");
--- weather-app/src/stores/weather.js
+++ weather-app/src/stores/weather.js
@@ -1,12 +1,27 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
+import axios from 'axios'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
+export const useWeatherStore = defineStore('weather', {
+  state: () => ({
+    api_key: '27bcc252742830381afd6856832da01a', 
+    
+    // baseURL: 'https://api.openweathermap.org/data/2.5/',
+    weather_icon: 'https://openweathermap.org/img/wn/', 
+    location_query: '', 
+    weather: '', 
+  }), 
+  getters:{}, 
+  actions:{
+  async  fetchweather(e){
+      if(e.key == 'Enter'){
+      const  response  = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${this.location_query}&units=metric&appid=${this.api_key}`);
+      };
+      console.log(response);
+    // this.weather = response; 
+
+    },
   }
 
-  return { count, doubleCount, increment }
+
 })
